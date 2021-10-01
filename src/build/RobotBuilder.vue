ebe499a16a702dac86fed8ce6939a39a59334e0f
Paul Fannon <fannonp@c02fm78vmd6m.wwt.com>
2021-10-01 14:08:50 -0500

    Add interoperable and conditional text feature

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -2,6 +2,10 @@
     <div>
         <div class="top-row">
         <div class="top part">
+            <div class="robot-name">
+                {{selectedRobot.head.title}}
+                <span v-if:="selectedRobot.head.onSale" class="sale">Sale!</span>
+            </div>
             <img :src="selectedRobot.head.src" title="head"/>
             <button @click="selectPreviousHead()" class="prev-selector">&#9668;</button>
             <button @click="selectNextHead()" class="next-selector">&#9658;</button>
@@ -213,5 +217,14 @@ export default {
 }
 .right .next-selector {
   right: -3px;
+}
+.robot-name {
+    position: absolute;
+    top: -25px;
+    text-align: center;
+    width: 100%;
+}
+.sale {
+    color: red;
 }
 </style>
