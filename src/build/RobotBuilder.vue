fb68d405251a142d2a2e91d597cc07ceab9d569b
Paul Fannon <fannonp@c02fm78vmd6m.wwt.com>
2021-10-04 15:18:18 -0500

    Add computed class styling to head component

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -2,7 +2,7 @@
     <div class="content">
         <button class="add-to-cart" @click="addToCart()">Add to Cart</button>
         <div class="top-row">
-            <div class="top part" :style="headBorderStyle">
+            <div :class="[saleBorderClass, 'top', 'part']">
                 <div class="robot-name">
                     {{selectedRobot.head.title}}
                     <span v-if:="selectedRobot.head.onSale" class="sale">Sale!</span>
@@ -94,6 +94,9 @@ export default {
     },
     headBorderStyle() {
       return { border: this.selectedRobot.head.onSale ? '3px solid red' : '3px solid #aaa' };
+    },
+    saleBorderClass() {
+      return this.selectedRobot.head.onSale ? 'sale-border' : '';
     },
   },
   methods: {
@@ -275,5 +278,8 @@ td, th {
 }
 .cost {
     text-align:right;
+}
+.sale-border {
+  border: 3px solid red;
 }
 </style>
