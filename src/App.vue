dd5594d588ebe0a854737c9a82004bcf4f3bd1ec
Paul Fannon <fannonp@c02fm78vmd6m.wwt.com>
2021-10-05 15:13:02 -0500

    Add Search component with composition API implementation

--- src/App.vue
+++ src/App.vue
@@ -10,18 +10,19 @@
     </nav>
   </header>
   <main>
-    <RobotBuilder />
+    <Search />
   </main>
 </template>
 
 <script>
 // import HomePage from './home/HomePage.vue';
-import RobotBuilder from './build/RobotBuilder.vue';
+// import RobotBuilder from './build/RobotBuilder.vue';
+import Search from './search/Search.vue';
 
 export default {
   name: 'App',
   components: {
-    RobotBuilder,
+    Search,
   },
 };
 </script>
