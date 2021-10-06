7aa02df6ffadf3ecc2002d354aaa25cb49bd98fe
Paul Fannon <fannonp@c02fm78vmd6m.wwt.com>
2021-10-06 15:57:03 -0500

    Add teleport element to render part description

--- src/build/PartsSelector.vue
+++ src/build/PartsSelector.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="part" :class="position" >
-    <img :src="selectedPart.src" title="arm"/>
+    <img :src="selectedPart.src" @click="showPartInfo = !showPartInfo" title="arm"/>
     <button @click="selectPreviousPart()" class="prev-selector"></button>
     <button @click="selectNextPart()" class="next-selector"></button>
     <span class="sale" v-show="selectedPart.onSale">Sale!</span>
+    <teleport to="#partInfo" v-if="showPartInfo">
+      <div v-if="showPartInfo">
+        <div>{{selectedPart.cost}} {{selectedPart.title}} {{selectedPart.type}}</div>
+        <div>{{selectedPart.description}}</div>
+        <hr />
+      </div>
+    </teleport>
   </div>
 </template>
 
@@ -33,7 +40,7 @@ export default {
     },
   },
   data() {
-    return { selectedPartIndex: 0 };
+    return { selectedPartIndex: 0, showPartInfo: false };
   },
   computed: {
     selectedPart() {
