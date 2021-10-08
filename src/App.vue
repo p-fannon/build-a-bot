a6e5d66b2b58b23c7ec5d9940e672390111c0f01
Paul Fannon <fannonp@c02fm78vmd6m.wwt.com>
2021-10-08 16:42:58 -0500

    Add sidebars via named views

--- src/App.vue
+++ src/App.vue
@@ -16,9 +16,15 @@
       </ul>
     </nav>
   </header>
-  <main>
-    <router-view/>
-  </main>
+  <div class="container">
+    <aside class="aside">
+      <router-view name="sidebar" />
+    </aside>
+    <main>
+      <!-- <router-view name="default" /> -->
+      <router-view />
+    </main>
+  </div>
 </template>
 
 <script>
@@ -43,15 +49,14 @@ body {
   color: red;
 }
 main {
-  margin: 0 auto;
   padding: 30px;
   background-color: white;
-  width: 1024px;
+  width: 964px;
   min-height: 300px;
 }
 header {
   background-color: #999;
-  width: 1084px;
+  width: 1184px;
   margin: 0 auto;
 }
 ul {
@@ -78,5 +83,16 @@ ul {
 } */
 .foo {
   color: white;
+}
+.container {
+  display: flex;
+  margin: 10px auto 0 auto;
+  justify-content: center;
+}
+.aside {
+  padding: 30px;
+  background-color: #aaa;
+  width: 100px;
+  min-height: 300px;
 }
 </style>
